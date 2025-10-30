0a471472f81e11e9193d407eddcb19c5a961f1cf
KROSSEYE <30990184+KROSSEYE@users.noreply.github.com>
2025-10-30 01:22:52 +0000

    Add release fetcher to namespace

--- src/godotkit/engine/__init__.py
+++ src/godotkit/engine/__init__.py
@@ -1,6 +1,24 @@
 from . import utils
 from .engine import GodotEngine
 from .exceptions import DownloadError, ExtractError
+from .release_fetcher import (
+    GodotAsset,
+    GodotFetcher,
+    GodotRelease,
+    detect_architecture,
+    detect_platform,
+)
 from .version_parsing import GodotVersion
 
-__all__ = ["utils", "GodotEngine", "DownloadError", "ExtractError", "GodotVersion"]
+__all__ = [
+    "utils",
+    "GodotEngine",
+    "DownloadError",
+    "ExtractError",
+    "GodotAsset",
+    "GodotFetcher",
+    "GodotRelease",
+    "detect_architecture",
+    "detect_platform",
+    "GodotVersion",
+]
